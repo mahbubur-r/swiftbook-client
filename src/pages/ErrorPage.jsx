d8bc43983efb8c334467b6b7591b1b61f884f977
mahbubur-r <mahbubur-r@outlook.com>
2025-12-09 03:21:42 +0100

    update error page layout and go back button

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import error404 from '../assets/error-404.png';
+import { FaBackwardStep } from "react-icons/fa6";
 
 const ErrorPage = () => {
     return (
-        <div className='text-center text-primary'>
+        <div className='text-center'>
             <img className='inline w-1/2 mt-10 rounded-4xl' src={error404} alt="" />
             {/* <h1 className='text-center text-5xl mt-10'>Oops, page not found!</h1> */}
-            <p className='  mt-10 text-3xl text-primary'>The page you are looking for is not available.</p>
-            <Link to='/'><button className='btn mt-5 text-2xl text-white bg-primary p-2 rounded-lg '>Go Back !</button></Link>
+            <p className='mt-10 text-2xl'>The page you are looking for is not available.</p>
+            <Link to='/'><button className='btn px-5 py-2 cursor-pointer mt-5 text-2xl text-white bg-primary items-center justify-center inline-flex rounded-lg gap-2'><FaBackwardStep />Go Back</button></Link>
+
         </div>
     );
 };
